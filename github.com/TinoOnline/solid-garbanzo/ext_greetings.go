5e7325291a7ef9dfac46a97a88da2fafb84fdd42
Tinotenda Kurimwi <tinokurimwi@motech.dev>
2024-07-31 11:16:35 +0200

    initial extended message commit

--- github.com/TinoOnline/solid-garbanzo/ext_greetings.go
+++ github.com/TinoOnline/solid-garbanzo/ext_greetings.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"math/rand"
 )
 
-func ExtendedMessages(messages []string) []string  {
-	extended := []string{}
-	for _, message := range(messages){
-		extended = append(extended, message + randomExtensions())
+// Extends the greetings with a follow up question
+func ExtendedMessages(messages map[string]string) (map[string]string, error)  {
+	extended := map[string]string{}
+	for key, message := range(messages){
+		if(message == ""){
+			return nil, errors.New("the message is empty")
+		}
+		extended[key] = fmt.Sprintf("%v %v",message, randomExtensions() ) 
 	}
-	return extended
+	return extended, nil
 }
 
 func randomExtensions() string {
